603fda76a5ac8fcd8427c29d762f712913ba71c5
Peng Chao <41148986+4184090@users.noreply.github.com>
2019-07-02 10:54:15 +0800

    Update BankCard.sql

--- src/main/resources/sql/BankCard.sql
+++ src/main/resources/sql/BankCard.sql
@@ -32,7 +32,6 @@ CREATE TABLE `BankCard`  (
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码',
   `report_loss` int(255) NULL DEFAULT NULL COMMENT '是否挂失',
   PRIMARY KEY (`card_id`) USING BTREE,
-  INDEX `fk_cust_id`(`cust_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
